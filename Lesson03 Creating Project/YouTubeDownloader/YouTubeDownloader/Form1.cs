c877d7e3bca3c5631c990926161f8c5248cbfc1f
mingming du <dafengdumingming@outlook.com>
2017-07-24 01:24:30 -0500

    download link normalized and validated

---
+++ Lesson03 Creating Project/YouTubeDownloader/YouTubeDownloader/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+
+namespace YouTubeDownloader
+{
+    public partial class frmYTDownloader : Form
+    {
+        private object folderBrowserDiaglod1;
+
+        public frmYTDownloader()
+        {
+            InitializeComponent();
+            cboFileType.SelectedIndex = 0; // set video as first choice 
+            // line below gets path to mydocuments
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            // set the path of browser dialog box 
+            folderBrowserDialog1.SelectedPath = folder;
+        }
+
+
+        private void btnDownloadFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+                txtDownloadFolder.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+
+            Tuple<bool, string> isLinkGood = ValidateLink();
+            MessageBox.Show("Is it good link ?" + isLinkGood.Item1 + "Link is : " + isLinkGood.Item2);
+
+
+
+            //// Our test youtube link ************************************************************************          
+            ////string link = "https://www.youtube.com/watch?v=xjo1FCW9_kM&list=RDpHdm3K2qEzU&index=22";
+            //string link = "https://www.youtube.com/watch?v=pv-6rweZR_s";
+
+
+            ///*
+            // * Get the available video formats.
+            // * We'll work with them in the video and audio download examples.
+            // */
+            //IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+
+
+            ////*Select the first .mp4 video with 360p resolution
+
+            //VideoInfo video = videoInfos.First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
+
+            ///*
+            // * If the video has a decrypted signature, decipher it
+            // */
+            //if (video.RequiresDecryption)
+            //{
+            //    DownloadUrlResolver.DecryptDownloadUrl(video);
+            //}
+
+            ///*
+            // * Create the video downloader.
+            // * The first argument is the video to download.
+            // * The second argument is the path to save the video file.
+            // */
+            //var videoDownloader = new VideoDownloader(video,
+            //    Path.Combine(txtDownloadFolder.Text, video.Title + video.VideoExtension));
+
+            //// Register the ProgressChanged event and print the current progress
+            //// videoDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage);
+
+            ///*
+            // * Execute the video downloader.
+            // * For GUI applications note, that this method runs synchronously.
+            // */
+            //videoDownloader.Execute();
+
+            //*******************************************************************************
+            //******************************************************************************
+
+            //           // Our test youtube link
+            //           string link = "https://www.youtube.com/watch?v=pHdm3K2qEzU&list=RDpHdm3K2qEzU";
+
+            //           /*
+            //            * Get the available video formats.
+            //            * We'll work with them in the video and audio download examples.
+            //            */
+            //           IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+
+            //           /*
+            //* We want the first extractable video with the highest audio quality.
+            //*/
+            //           VideoInfo video = videoInfos
+            //               .Where(info => info.CanExtractAudio)
+            //               .OrderByDescending(info => info.AudioBitrate)
+            //               .FirstOrDefault();
+
+            //           /*
+            //            * If the video has a decrypted signature, decipher it
+            //            */
+            //           if (video.RequiresDecryption)
+            //           {
+            //               DownloadUrlResolver.DecryptDownloadUrl(video);
+            //           }
+
+            //           /*
+            //            * Create the audio downloader.
+            //            * The first argument is the video where the audio should be extracted from.
+            //            * The second argument is the path to save the audio file.
+            //            */
+            //           var audioDownloader = new AudioDownloader(video, Path.Combine(txtDownloadFolder.Text, video.Title + video.AudioExtension));
+
+            //           // Register the progress events. We treat the download progress as 85% of the progress and the extraction progress only as 15% of the progress,
+            //           // because the download will take much longer than the audio extraction.
+            //           //audioDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage * 0.85);
+            //           //audioDownloader.AudioExtractionProgressChanged += (sender, args) => Console.WriteLine(85 + args.ProgressPercentage * 0.15);
+
+            //           /*
+            //            * Execute the audio downloader.
+            //            * For GUI applications note, that this method runs synchronously.
+            //            */
+            //           audioDownloader.Execute();
+            //       }
+
+            //       private void frmYTDownloader_Load(object sender, EventArgs e)
+            //       {
+
+            //       }
+
+            //       private void txtLink_TextChanged(object sender, EventArgs e)
+            //       {
+
+            //       }
+
+            //       private void cboFileType_SelectedIndexChanged(object sender, EventArgs e)
+            //       {
+
+            //       }
+
+            //       private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+            //       {
+
+            //       }
+
+            // Our test youtube link
+
+        }
+
+        private Tuple<bool, string> ValidateLink()
+        {
+            string normalURL;
+            if(DownloadUrlResolver.TryNormalizeYoutubeUrl(txtLink.Text,out normalURL))
+            {
+                return Tuple.Create(true, normalURL); // return true and acutla link if succesfull
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid youtube link ");
+                return Tuple.Create(false, "");
+            }
+        }
+    }
+}
---
+++ Lesson03 Creating Project/YouTubeDownloader/YouTubeDownloader/YouTubeModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExtractor;
+
+namespace YouTubeDownloader
+{
+    // download (YouTubeVideoModel)
+    public class YouTubeModel
+    {
+        public IEnumerable<VideoInfo> VideoInfo { get; set; }
+        public string FolderPath { get; set; }    // store the folder we designated in folder Browsing 
+        public string Link { get; set; }          // youtube link we tpye in   
+        public string FilePath { get; set; }        // store the folder location + name of file and its extension
+        public VideoInfo Video { get; set; }            // ojbect is from Youtube Extractor and contains info about the file to be download
+    }
+
+    public class YouTubeVideoModel : YouTubeModel
+    {
+        public  VideoDownloader VideoDownloaderType { get; set; }
+        // object contain the excute methode , so wwe can download files 
+        // DownloadFinished Event  we can handle when a download is finished 
+        // we will use this  to enable acess to the interface after it is disabled while a file downlaods
+        // DownloadProgressChange event with which updates the progress bar as a video downloads
+
+    }
+
+    public class YouTubeAudioModel : YouTubeModel
+    {
+        public AudioDownloader AudioDownloaderType { get; set; }
+        // object that contian 
+
+        // DownloadFinished Event  we can handle when a download is finished 
+        // we will use this  to enable acess to the interface after it is disabled while a file downlaods
+        // DownloadProgressChange event with which updates the progress bar as a video downloads
+    }
+
+}
